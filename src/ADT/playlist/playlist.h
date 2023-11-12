14fca81d07b065acb99854e77cec1401c7bb4659
rhleena <lina.hartono06@gmail.com>
2023-11-12 12:32:29 +0700

    Create playlist.h

---
+++ src/ADT/playlist/playlist.h
@@ -0,0 +1,150 @@
+/* File : playlist.h */
+/* contoh ADT list berkait dengan representasi fisik pointer  */
+/* Representasi address dengan pointer */
+/* infotype adalah integer */
+
+#ifndef playlist_H
+#define playlist_H
+
+#include "boolean.h"
+#include "../LineMachine/linemachine.h"
+
+#define NilPlaylist NULL
+
+typedef int idAlbum;
+typedef tElmtlist *addressLagu;
+
+typedef Kalimat infotype;
+typedef struct tElmtlist *address;
+
+typedef struct tElmtlist { 
+    Kalimat NamaPenyanyi;
+    Kalimat NamaAlbum;
+    Kalimat JudulLagu;
+    addressLagu next;
+} ElmtList;
+
+typedef struct {
+    addressLagu First;
+} List;
+
+/* Definisi list : */
+/* List kosong : First(L) = Nil */
+/* Setiap elemen dengan address P dapat diacu Info(P), Next(P) */
+/* Elemen terakhir list : jika addressnya Last, maka Next(Last)=Nil */
+#define InfoLagu(P) (P)->infoLagu
+#define NextLagu(P) (P)->next
+#define FirstLagu(L) ((L).First)
+
+/* PROTOTYPE */
+/****************** TEST LIST KOSONG ******************/
+boolean IsEmpty (List L);
+/* Mengirim true jika list kosong */
+
+/****************** PEMBUATAN LIST KOSONG ******************/
+void CreateEmptyPlaylist (List *L);
+/* I.S. sembarang             */
+/* F.S. Terbentuk list kosong */
+
+/****************** Manajemen Memori ******************/
+address AlokasiPlaylist (infotype X);
+/* Mengirimkan address hasil alokasi sebuah elemen */
+/* Jika alokasi berhasil, maka address tidak nil, dan misalnya */
+/* menghasilkan P, maka info(P)=X, Next(P)=Nil */
+/* Jika alokasi gagal, mengirimkan Nil */
+void DealokasiPlaylist (addressLagu *P);
+/* I.S. P terdefinisi */
+/* F.S. P dikembalikan ke sistem */
+/* Melakukan dealokasi/pengembalian address P */
+
+/****************** PENCARIAN SEBUAH ELEMEN LIST ******************/
+address SearchLagu (List L, infotype X);
+/* Mencari apakah ada elemen list dengan info(P)= X */
+/* Jika ada, mengirimkan address elemen tersebut. */
+/* Jika tidak ada, mengirimkan Nil */
+
+/****************** PRIMITIF BERDASARKAN NILAI ******************/
+/*** PENAMBAHAN ELEMEN ***/
+void InsVFirstLagu (List *L, infotype X);
+/* I.S. L mungkin kosong */
+/* F.S. Melakukan alokasi sebuah elemen dan */
+/* menambahkan elemen pertama dengan nilai X jika alokasi berhasil */
+void InsVLastLagu (List *L, infotype X);
+/* I.S. L mungkin kosong */
+/* F.S. Melakukan alokasi sebuah elemen dan */
+/* menambahkan elemen list di akhir: elemen terakhir yang baru */
+/* bernilai X jika alokasi berhasil. Jika alokasi gagal: I.S.= F.S. */
+
+/*** PENGHAPUSAN ELEMEN ***/
+void DelVFirstLagu (List *L, infotype *X);
+/* I.S. List L tidak kosong  */
+/* F.S. Elemen pertama list dihapus: nilai info disimpan pada X */
+/*      dan alamat elemen pertama di-dealokasi */
+void DelVLastLagu (List *L, infotype *X);
+/* I.S. list tidak kosong */
+/* F.S. Elemen terakhir list dihapus: nilai info disimpan pada X */
+/*      dan alamat elemen terakhir di-dealokasi */
+
+/****************** PRIMITIF BERDASARKAN ALAMAT ******************/
+/*** PENAMBAHAN ELEMEN BERDASARKAN ALAMAT ***/
+void InsertFirstLagu (List *L, addressLagu P);
+/* I.S. Sembarang, P sudah dialokasi  */
+/* F.S. Menambahkan elemen ber-address P sebagai elemen pertama */
+void InsertAfterLagu (List *L, addressLagu P, addressLagu Prec);
+/* I.S. Prec pastilah elemen list dan bukan elemen terakhir, */
+/*      P sudah dialokasi  */
+/* F.S. Insert P sebagai elemen sesudah elemen beralamat Prec */
+void InsertLastLagu (List *L, addressLagu P);
+/* I.S. Sembarang, P sudah dialokasi  */
+/* F.S. P ditambahkan sebagai elemen terakhir yang baru */
+
+/*** PENGHAPUSAN SEBUAH ELEMEN ***/
+void DelFirstLagu (List *L, addressLagu *P);
+/* I.S. List tidak kosong */
+/* F.S. P adalah alamat elemen pertama list sebelum penghapusan */
+/*      Elemen list berkurang satu (mungkin menjadi kosong) */
+/* First element yg baru adalah suksesor elemen pertama yang lama */
+void DelPLagu (List *L, infotype X);
+/* I.S. Sembarang */
+/* F.S. Jika ada elemen list beraddress P, dengan info(P)=X  */
+/* Maka P dihapus dari list dan di-dealokasi */
+/* Jika tidak ada elemen list dengan info(P)=X, maka list tetap */
+/* List mungkin menjadi kosong karena penghapusan */
+void DelLastLagu (List *L, addressLagu *P);
+/* I.S. List tidak kosong */
+/* F.S. P adalah alamat elemen terakhir list sebelum penghapusan  */
+/*      Elemen list berkurang satu (mungkin menjadi kosong) */
+/* Last element baru adalah predesesor elemen terakhir yg lama, */
+/* jika ada */
+void DelAfterLagu (List *L, addressLagu *Pdel, addressLagu Prec);
+/* I.S. List tidak kosong. Prec adalah anggota list  */
+/* F.S. Menghapus Next(Prec): */
+/*      Pdel adalah alamat elemen list yang dihapus  */
+
+/****************** PROSES SEMUA ELEMEN LIST ******************/
+void PrintInfoLagu (List L);
+/* I.S. List mungkin kosong */
+/* F.S. Jika list tidak kosong, iai list dicetak ke kanan: [e1,e2,...,en] */
+/* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
+/* Jika list kosong : menulis [] */
+/* Tidak ada tambahan karakter apa pun di awal, akhir, atau di tengah */
+int NbElmtLagu (List L);
+/* Mengirimkan banyaknya elemen list; mengirimkan 0 jika list kosong */
+
+/****************** PROSES TERHADAP LIST ******************/
+
+void InversList (List *L);
+/* I.S. sembarang. */
+/* F.S. elemen list dibalik : */
+/* Elemen terakhir menjadi elemen pertama, dan seterusnya. */
+/* Membalik elemen list, tanpa melakukan alokasi/dealokasi. */
+
+void Konkat1Lagu (List *L1, List *L2, List *L3);
+/* I.S. L1 dan L2 sembarang */
+/* F.S. L1 dan L2 kosong, L3 adalah hasil konkatenasi L1 & L2 */
+/* Konkatenasi dua buah list : L1 dan L2    */
+/* menghasilkan L3 yang baru (dengan elemen list L1 dan L2) */
+/* dan L1 serta L2 menjadi list kosong.*/
+/* Tidak ada alokasi/dealokasi pada prosedur ini */
+
+#endif
