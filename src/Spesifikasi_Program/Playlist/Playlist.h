d42350f59bb013de040aa8c33b8f9c2aeedd67e1
AhmadFawwazi <ahmadfawwazi2019@gmail.com>
2023-11-12 14:03:13 +0700

    playlist apdet

---
+++ src/Spesifikasi_Program/Playlist/Playlist.h
@@ -0,0 +1,11 @@
+#ifndef __PLAYLIST_H__
+#define __PLAYLIST_H__
+
+#include "../../ADT/LinkedList/LinkedList.h"
+#include <stdio.h>
+#include "../../ADT/Stack/RiwayatLagu.h"
+#include "../../ADT/array/arraydinamis.h"
+#include "../../ADT/LineMachine/linemachine.h"
+#include "../../ADT/pcolor/pcolor.h"
+
+#endif
